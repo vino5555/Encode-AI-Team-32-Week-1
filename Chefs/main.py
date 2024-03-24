6a7cb3037e0e6fe7b3b49f625b0ebabb64e29845
Kanaya Ozora <kanayaozora@gmail.com>
2024-03-24 19:54:38 +0100

    Update: IndonesianChef.py and Chef folder

---
+++ Chefs/main.py
@@ -0,0 +1,23 @@
+#Calls the chef script based on personality
+def chef_script(personality):
+    if personality == "indonesian_chef":
+        import indonesian_chef
+        indonesian_chef.run()
+    elif personality == "university_chef":
+        import university_chef
+        university_chef.run()
+    else:
+        print("Sorry, this personality is invalid.")
+
+if __name__ == "__main__":
+    print("Choose one of the following chef: ")
+    print("1. An energetic Indonesian chef")
+    print("2. A tired UK university cook with a Scottish accent")
+    personality = input("Type in the number corresponding to the chef of your choice: ")
+
+    if personality == "1":
+        chef_script("indonesian_chef")
+    elif personality == "2":
+        chef_script("university_chef")
+    else:
+        print("Sorry, this number is out of bounds.")
---
+++ Chefs/university_chef.py
@@ -0,0 +1,74 @@
+from openai import OpenAI
+
+api_key = " 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"
+
+client = OpenAI(api_key=api_key)
+
+messages = [
+    {
+        "role": "system",
+        "content": "You are a tired but experienced cook from the UK who cooks meals for university students. You always talk with a refined vocabulary, but you use a Scottish accent. Since the students come from different backgrounds, you are knowledgeable about various cuisines from different parts of the world, but wouldn’t be experienced when it comes to luxurious dishes and complex cooking techniques. You also don't know any Korean dishes. Students have a difficult time understanding your recipe instructions. ",
+    }
+]
+messages.append(
+    {
+        "role": "system",
+        "content": "Your client is going to ask for one of the three following prompts: suggesting dishes based on ingredients, giving recipes to dishes, or criticizing recipes given by the user input. If your client passes a different prompt than these three scenarios as the first message, you should deny the request and ask to try again. If your client passes a Korean dish, you should deny this input and say you need to improve in your Korean knowledge. If your client passes one or more ingredients, you should only suggest several dish names that can be made with these ingredients. Suggest the dish name only, and do not describe the recipe of the dishes. If your client passes a dish name, you should give a recipe for that dish. If your client passes a recipe for a dish, you should criticize the recipe and suggest changes. If a message is empty do not attempt to guess what your client would possibly say, it’s literally empty.",
+    }
+)
+
+dish = input("Type in your input:\n")
+messages.append(
+    {
+        "role": "user",
+        "content": f"If {dish} is a list of ingredients, suggest me 3 to 5 dishes that I can create without any instructions or recipes. If {dish} is a specific dish, suggest me a detailed recipe and the preparation steps for making {dish}. If {dish} is a recipe, criticize my recipe and suggest changes. "
+    }
+)
+
+model = "gpt-3.5-turbo"
+
+stream = client.chat.completions.create(
+    model=model,
+    messages=messages,
+    stream=True,
+)
+
+collected_messages = []
+for chunk in stream:
+    chunk_message = chunk.choices[0].delta.content or ""
+    print(chunk_message, end="")
+    collected_messages.append(chunk_message)
+
+messages.append(
+    {
+        "role": "system",
+        "content": "".join(collected_messages)
+    }
+)
+
+while True:
+    print("\n")
+    user_input = input()
+    messages.append(
+        {
+            "role": "user",
+            "content": user_input
+        }
+    )
+    stream = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        stream=True,
+    )
+    collected_messages = []
+    for chunk in stream:
+        chunk_message = chunk.choices[0].delta.content or ""
+        print(chunk_message, end="")
+        collected_messages.append(chunk_message)
+
+    messages.append(
+        {
+            "role": "system",
+            "content": "".join(collected_messages)
+        }
+    )
