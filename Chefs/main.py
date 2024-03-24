84b023ca5e32a5a123d5bae79fdac35507f08eff
Kanaya Ozora <kanayaozora@gmail.com>
2024-03-24 20:09:28 +0100

    Update: IndonesianChef.py and Chef folder

--- Chefs/main.py
+++ Chefs/main.py
@@ -1,4 +1,4 @@
-#Calls the chef script based on personality
+
 def chef_script(personality):
     if personality == "indonesian_chef":
         import indonesian_chef
@@ -9,16 +9,16 @@ def chef_script(personality):
     else:
         print("Sorry, this personality is invalid.")
 
-#User is asked to type in a number based on the chef they would like to discuss their recipes/dishes with
 if __name__ == "__main__":
-    print("Choose one of the following chef: ")
-    print("1. An energetic Indonesian chef")
-    print("2. A tired UK university cook with a Scottish accent")
-    personality = input("Type in the number corresponding to the chef of your choice: ")
+    while True:
+        print("Choose one of the following chefs:")
+        print("1. An energetic Indonesian chef")
+        print("2. A tired UK university cook with a Scottish accent")
+        personality = input("Type in the number corresponding to the chef of your choice: ")
 
-    if personality == "1":
-        chef_script("indonesian_chef")
-    elif personality == "2":
-        chef_script("university_chef")
-    else:
-        print("Sorry, this number is out of bounds.")
+        if personality == "1":
+            chef_script("indonesian_chef")
+        elif personality == "2":
+            chef_script("university_chef")
+        else:
+            print("Sorry, this number is out of bounds.")
